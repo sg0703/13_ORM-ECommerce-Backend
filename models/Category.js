53818ec332817a515343fdcf701bbb3afe7bee59
sg0703 <samgates@Sams-MacBook-Pro.local>
2021-04-28 14:29:30 -0400

    Initial commit

---
+++ models/Category.js
@@ -0,0 +1,30 @@
+const { Model, DataTypes } = require('sequelize');
+
+const sequelize = require('../config/connection.js');
+
+class Category extends Model {}
+
+// added id and category_name as defined in readme
+Category.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    category_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    }
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'category',
+  }
+);
+
+module.exports = Category;
